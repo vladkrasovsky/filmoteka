364bad6d10cadc9c684bca332d9bf1bbc53b5b29
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2022-11-29 19:52:56 +0200

    Hotfix to accept pull request

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,44 +1,40 @@
-import Pagination from 'tui-pagination'; 
-
-// const Pagination = tui.Pagination;
-
+import Pagination from 'tui-pagination';
 
 const container = document.getElementById('tui-pagination-container');
-const instance = new Pagination(container, { 
-    totalItems: 500,
-    itemsPerPage: 10,
-    visiblePages: 10,
-    page: 1,
-    centerAlign: true,
-    firstItemClassName: 'tui-first-child',
-    lastItemClassName: 'tui-last-child',
-    usageStatistics: false,
-
-
-
-
+const instance = new Pagination(container, {
+  totalItems: 500,
+  itemsPerPage: 10,
+  visiblePages: 10,
+  page: 1,
+  centerAlign: true,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  usageStatistics: false,
 });
 function createPage(page) {
-    return page.map({
-        page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-        currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-        moveButton:
-            '<a href="#" class="tui-page-btn tui-{{type}}">' +
-                '<span class="tui-ico-{{type}}">{{type}}</span>' +
-            '</a>',
-        disabledMoveButton:
-            '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-                '<span class="tui-ico-{{type}}">{{type}}</span>' +
-            '</span>',
-        moreButton:
-            '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-                '<span class="tui-ico-ellip">...</span>' +
-            '</a>'
-    })
+  return page.map({
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  });
 }
 
-// const pagination = new Pagination(container, options);
-
+const pagination = new Pagination(container, {
+  //   totalItems: 500,
+  //   itemsPerPage: 20,
+  //   visiblePages: 5,
+});
 
 export { pagination };
-
