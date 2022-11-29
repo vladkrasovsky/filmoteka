1d205932e673d6e74cbb5d5322d63768d3b6f61e
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2022-11-29 14:38:12 +0200

    Changes name

--- src/js/modalTeam.js
+++ src/js/modalTeam.js
@@ -5,13 +5,12 @@ import scrumMasterObj from '../images/team_photos/webp/Veronika_Moskaliuk.webp';
 import devStanyslavObj from '../images/team_photos/webp/Stanyslav_Cherniakov.webp';
 import devViktoriiaObj from '../images/team_photos/webp/Aleksieieva_Viktoriia.webp';
 import devDenysObj from '../images/team_photos/webp/Denys_Korsukov.webp';
-import devIrynaObj from '../images/team_photos/webp/Iryna_Hridnova.webp';
+import devIrynaObj from '../images/team_photos/webp/Iryna_Diukova.webp';
 import devYanaObj from '../images/team_photos/webp/Lizogub_Yana.webp';
 import devMariiaObj from '../images/team_photos/webp/Mariia_Prokopenko.webp';
 import devOleksiiObj from '../images/team_photos/webp/Oleksii_Zablotskyi.webp';
 import devSerhiiDenObj from '../images/team_photos/webp/Serhii_Denysiuk.webp';
 import devSerhiiPadalkaObj from '../images/team_photos/webp/Serhii_Padalka.webp';
-
 
 const team = [
   {
@@ -72,7 +71,7 @@ const team = [
 ];
 
 const markupCard = team
-  .map(({ photo, name, role}) => {
+  .map(({ photo, name, role }) => {
     return `<li class="team_card">
              
                <img loading="lazy" src="${photo}" alt="${name}" class="team_photo">
