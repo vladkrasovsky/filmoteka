1eb4946724804af6df67233c26fe33683aa23920
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2022-12-01 09:14:34 +0200

    Update library.js

--- src/js/library.js
+++ src/js/library.js
@@ -1,40 +1,34 @@
 import LsService from './API/storage';
+import { storageKey } from './constants';
 import { createMarkup } from './createMarkup';
 import { getLocalStorageData } from './localStorage';
-import trendings from '../data/movies/trendings.json';
 import * as modalMovie from './modalMovie';
-
-getLocalStorageData();
 
 const refs = {
   moviesList: document.querySelector('.moviesListRef'),
   watchedBtn: document.querySelector('button[data-btn-watched]'),
   queueBtn: document.querySelector('button[data-btn-queue]'),
-  // watchedRef: document.querySelector('.btn-watched'),
-  // queueRef: document.querySelector('.btn-queue'),
 };
 
-const WATCHED_KEY = 'watched-list';
-const QUEUE_KEY = 'queue-list';
-
+getLocalStorageData();
 onWatched();
 
 refs.watchedBtn.addEventListener('click', onWatched);
 refs.queueBtn.addEventListener('click', onQueue);
-// refs.watchedRef.addEventListener('click', onWatched);
-// refs.queueRef.addEventListener('click', onQueue);
 
 function onWatched() {
-  const movies = LsService.load(WATCHED_KEY);
-  refs.moviesList.innerHTML = createMarkup(trendings.results);
+  const movies = LsService.load(storageKey.WATCHED_LIST);
+  refs.moviesList.innerHTML = createMarkup(movies);
+
   refs.queueBtn.classList.remove('header-btn-active');
   refs.watchedBtn.classList.add('header-btn-active');
   refs.watchedBtn.disabled = true;
 }
 
 function onQueue() {
-  const movies = LsService.load(QUEUE_KEY);
-  refs.moviesList.innerHTML = createMarkup(trendings.results);
+  const movies = LsService.load(storageKey.QUEUE_LIST);
+  refs.moviesList.innerHTML = createMarkup(movies);
+
   refs.queueBtn.classList.add('header-btn-active');
   refs.watchedBtn.classList.remove('header-btn-active');
   refs.watchedBtn.disabled = false;
