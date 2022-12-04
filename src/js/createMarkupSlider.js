daf4374c8afc17e8f4ba45683b9c7e40de78ec62
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2022-12-04 16:05:56 +0200

    Update swiper slider

--- src/js/createMarkupSlider.js
+++ src/js/createMarkupSlider.js
@@ -6,17 +6,15 @@ export function createMarkupSlider(movieUa) {
 
     const poster_url = poster_path
       ? config.POSTER_BASE_URL + poster_path
-        : config.POSTER_PLACEHOLDER_URL;
-      
-    //   ${original_title}
+      : config.POSTER_PLACEHOLDER_URL;
 
     return (
       acc +
-        `<div class="swiper-slide" data-id='${id}'>
-        <a class='movies__link' href='' data-id='${id}'>
-      <img class="movies__image" src='${poster_url}' alt='${original_title}'>
-      <p class='movies__title'>${original_title}</p>
-        </div>`
+      `<div class="swiper-slide" data-id="${id}">
+        <a class="movies__link" href="" data-id="${id}">
+        <img class="movies__image" src="${poster_url}" alt="${original_title}">
+        <p class="movies__title">${original_title}</p>
+      </div>`
     );
   }, '');
 }
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,52 +1,39 @@
-import Swiper, { Navigation } from 'swiper';
-
-// init Swiper:
+import Swiper, { Navigation, Autoplay } from 'swiper';
 
 export function onSlider() {
   const swiper = new Swiper('.swiper', {
-  modules: [Navigation],
-  loop: true,
-  initialSlide:0,
-  slidesPerGroup: 1,
-  autoHeight: true,
+    modules: [Navigation, Autoplay],
+    loop: true,
+    initialSlide: 0,
+    slidesPerGroup: 1,
+    autoHeight: !true,
+    autoplay: true,
 
-  slidesPerView: 2,
-  spaceBetween: 20,
+    slidesPerView: 2,
+    spaceBetween: 20,
 
-  breakpoints: {
-    480: {
-      slidesPerView: 3,
-      spaceBetween: 20,
+    breakpoints: {
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+
+      768: {
+        slidesPerView: 5,
+        spaceBetween: 10,
+      },
+
+      1280: {
+        slidesPerView: 8,
+        spaceBetween: 10,
+      },
     },
 
-    768: {
-      slidesPerView: 5,
-      spaceBetween: 10,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
 
-    1280: {
-      slidesPerView: 8,
-      spaceBetween: 10,
-    },
-  },
-
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // virtual: {
-  //   slides: (function () {
-  //     let slide = []
-  //     for (let i = 0; i < 300; i++) {
-  //       slide.push(`<div class = "swiper-slide__movie">Слайд №${i}</div>`);
-  //     }
-  //     return slide;
-  //   }()),
-  // },
-
-  slideToClickedSlide: true,
-
-});
+    slideToClickedSlide: !true,
+  });
 }
-
