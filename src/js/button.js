0d128f26c6eed1a32f99f25e7910058336653a87
YanaLiz <105737326+YanaLiz@users.noreply.github.com>
2022-12-04 12:03:02 +0200

    add

--- src/js/button.js
+++ src/js/button.js
@@ -1,27 +1,31 @@
 
 
-window.onload = function(){
+window.onload = function () {
 	//window.scrollTo(x,y)
 	var scrolled;
 	var timer;
+	
 
-	document.getElementById('top').onclick = function(){
+	document.getElementById('top').onclick = function () {
 		scrolled = window.pageYOffset;
-		window.scrollTo(0,0);
-		scrollToTop();
-	}
-	function scrollToTop(){
-		if (scrolled > 0) {
-			window.scrollTo(0, scrolled);
-			scrolled = scrolled - 50; 
-			timer = setTimeout(scrollToTop, 200);
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth'
+		});
+		function scrollToTop() {
+			if (scrolled > 0) {
+				window.scrollTo(0, scrolled);
+				scrolled = scrolled - 50;
+				timer = setTimeout(scrollToTop, 200);
+			}
+			else {
+				clearTimeout(timer);
+				window.scrollTo(0, 0);
+			}
 		}
-		else {
-			clearTimeout(timer);
-			window.scrollTo(0,0);
-		}
 	}
+
+
+	// export default scrollToTop;
+
 }
-// export default scrollToTop;
-
-
