c1501969cd94df0bf4d3f7783a6b1f68c0af5d46
YanaLiz <105737326+YanaLiz@users.noreply.github.com>
2022-12-04 21:35:04 +0200

    add

--- src/js/button.js
+++ src/js/button.js
@@ -1,12 +1,14 @@
 
-
 const button = document.querySelector('#top')
-console.log(button)
+
+window.addEventListener('scroll', function() {
+      button.hidden = (pageYOffset < document.documentElement.clientHeight);
+    });
 
 window.onload = function () {
-	//window.scrollTo(x,y)
-	var scrolled;
-	var timer;
+	
+	let scrolled;
+	let timer;
 	
 	document.getElementById('top').onclick = function () {
 		scrolled = window.pageYOffset;
@@ -14,19 +16,10 @@ window.onload = function () {
 			top: 0,
 			behavior: 'smooth'
 		});
-		function scrollToTop() {
-			if (scrolled > 0) {
-				window.scrollTo(0, scrolled);
-				scrolled = scrolled - 50;
-				timer = setTimeout(scrollToTop, 200);
-				button.classList.remove('.visually-hidden')
-			}
-			else {
-				clearTimeout(timer);
-				window.scrollTo(0, 0);
-				return false;
-			}
-		}
+
 	}
 
 }
+
+
+
