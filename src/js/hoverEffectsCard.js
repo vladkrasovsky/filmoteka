ab73d2809e94802a0e7cdb2ff2e10e28a7831a52
Padalka Sergey <serega443@gmail.com>
2022-12-04 19:45:50 +0200

    убрал лишний код

--- src/js/hoverEffectsCard.js
+++ src/js/hoverEffectsCard.js
@@ -1,33 +1,12 @@
 import { Themoviedb } from './API/Themoviedb';
-// import { Spinner } from 'spin.js';
 
 const movieAboutText = new Themoviedb();
 let getEl = selector => document.querySelector(selector);
 const movieList = getEl('.movies__list');
 
-// var opts = {
-//   lines: 12, // The number of lines to draw
-//   length: 58, // The length of each line
-//   width: 21, // The line thickness
-//   radius: 54, // The radius of the inner circle
-//   scale: 0.3, // Scales overall size of the spinner
-//   corners: 1, // Corner roundness (0..1)
-//   speed: 1.1, // Rounds per second
-//   rotate: 0, // The rotation offset
-//   animation: 'spinner-line-shrink', // The CSS animation name for the lines
-//   direction: 1, // 1: clockwise, -1: counterclockwise
-//   color: '#ffffff', // CSS color or array of colors
-//   fadeColor: 'transparent', // CSS color or array of colors
-//   top: '50%', // Top position relative to parent
-//   left: '50%', // Left position relative to parent
-//   shadow: '0 0 1px transparent', // Box-shadow for the lines
-//   zIndex: 2000000000, // The z-index (defaults to 2e9)
-//   className: 'spinner', // The CSS class to assign to the spinner
-//   position: 'absolute', // Element positioning
-// };
-
-// var target = document.querySelector('.movies__item');
-// var spinner = new Spinner(opts).spin(target);
+let overlayText = null;
+let movieOverlay = null;
+let overlayVote = null;
 
 movieList.addEventListener('mouseover', onHoverCard);
 movieList.addEventListener('mouseout', onNotHoverCard);
@@ -36,13 +15,10 @@ function onHoverCard(e) {
   if (e.target.nodeName !== 'LI') {
     return;
   }
-  // let numId = e.target.getAttribute('data-id');
+
   fetchHoverEffect(e.target.getAttribute('data-id'));
 }
 
-let overlayText = null;
-let movieOverlay = null;
-let overlayVote = null;
 async function fetchHoverEffect(movieId) {
   movieAboutText.movieId = movieId;
 
@@ -55,20 +31,12 @@ async function fetchHoverEffect(movieId) {
     if (overlayVote === 0) {
       boxOverlay.insertAdjacentHTML(
         'beforeend',
-        `<div class="movies__overlay">
-            <p class="movies__overlay-vote">Vote: no ratings</p>
-            <p class="movies__overlay-about">About:</p>
-            <p class="movies__overlay-txt">${overlayText}</p>
-          </div>`
+        markupOverlayCardNoVote(overlayText)
       );
     } else {
       boxOverlay.insertAdjacentHTML(
         'beforeend',
-        `<div class="movies__overlay">
-            <p class="movies__overlay-vote">Vote: ${overlayVote}</p>
-            <p class="movies__overlay-about">About:</p>
-            <p class="movies__overlay-txt">${overlayText}</p>
-          </div>`
+        markupOverlayCard(overlayVote, overlayText)
       );
     }
 
@@ -78,25 +46,25 @@ async function fetchHoverEffect(movieId) {
   }
 }
 
-// function markupOverlayCardNoVote() {
-//   return `<div class="movies__overlay">
-//             <p class="movies__overlay-vote">Vote: no ratings</p>
-//             <p class="movies__overlay-about">About:</p>
-//             <p class="movies__overlay-txt">${overlayText}</p>
-//           </div>`;
-// }
-
-// function markupOverlayCard() {
-//   return `<div class="movies__overlay">
-//             <p class="movies__overlay-vote">Vote: ${overlayVote}</p>
-//             <p class="movies__overlay-about">About:</p>
-//             <p class="movies__overlay-txt">${overlayText}</p>
-//           </div>`;
-// }
-
 function onNotHoverCard(e) {
   if (e.target.nodeName !== 'LI') {
     return;
   }
   movieOverlay.remove();
+}
+
+function markupOverlayCardNoVote(overlayText) {
+  return `<div class="movies__overlay">
+            <p class="movies__overlay-vote">Vote: no ratings</p>
+            <p class="movies__overlay-about">About:</p>
+            <p class="movies__overlay-txt">${overlayText}</p>
+          </div>`;
+}
+
+function markupOverlayCard(overlayVote, overlayText) {
+  return `<div class="movies__overlay">
+            <p class="movies__overlay-vote">Vote: ${overlayVote}</p>
+            <p class="movies__overlay-about">About:</p>
+            <p class="movies__overlay-txt">${overlayText}</p>
+          </div>`;
 }
